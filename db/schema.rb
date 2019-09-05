79fdba4b92894fcc03e40d3b9e7cc2ba1c6b11c4
kanta-sato <kanta_sato@waku-2.com>
2019-09-05 11:25:13 +0900

    home create

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_29_032127) do
+ActiveRecord::Schema.define(version: 2019_09_05_020458) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,12 @@ ActiveRecord::Schema.define(version: 2019_07_29_032127) do
   end
 
   create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "homes", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
