c2693511ed68c191cf25b0b87f1fc55ce1248135
kanta-sato <kanta_sato@waku-2.com>
2019-09-30 18:05:56 +0900

    Add:has_secure_passwordカラム追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :homes
   resources :articles
   get "/" => "homes#index"
